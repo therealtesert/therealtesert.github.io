b285545c44b308b4cf31eb2dc37842a9662baac8
therealtesert <eeee11794@gmail.com>
2024-08-14 16:44:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// script.js
-
-// Function to get the Unix timestamp
 function getUnixTimestamp() {
   return Math.floor(Date.now() / 1000);
 }
 
-// Update the timestamp every second
 setInterval(function() {
-  // Get the timestamp
   const timestamp = getUnixTimestamp();
-  
-  // Update the paragraph element with the timestamp
+
   document.getElementById("timestamp").textContent = "Current Unix Timestamp: " + timestamp;
 }, 1000);
 
@@ -21,5 +15,5 @@ const resultParagraph = document.querySelector('p');
 button.addEventListener('click', () => {
   const base10 = parseInt(input.value, 10);
   const base14 = base10.toString(14);
-  resultParagraph.textContent = `base 14 number: ${base14}`;
+  document.getElementById("be14").textContent = `base 14 number: ${base14}`;
 });
